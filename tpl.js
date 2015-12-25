d69c6367faeeb484b410dc1f66c2ca851d733de8
lloydzhou <lloydzhou@qq.com>
2015-12-26 01:50:15 +0800

    add test case in index.html

--- tpl.js
+++ tpl.js
@@ -2,11 +2,11 @@
  * copyright @Lloyd Zhou(lloydzhou@qq.com)
  */
 ;(function (window) {
-    var FN = {}, t = {"\\": "\\\\", "\n": "\\n", "\r": "\\r", "'": "\\'",
+    var FN = {}, t = {"\\": "\\\\", "\n": "\\n", "\r": "\\r",
         "{{": "');o.push(", "}}": ");o.push('", "{%": "'); ", "%}": "\no.push('"}
     window.render = function(tmpl, data) {
         FN[tpl = tmpl || ''] = FN[tpl] || new Function("_", "var o = [];with(_){ \no.push('" + 
-            tmpl.replace(/([\\\n\r']|{{|}}|{%|%})/g, function(c) {return t[c]}) + "');} return o.join('')")
+            tmpl.replace(/([\\\n\r]|{{|}}|{%|%})/g, function(c) {return t[c]}) + "');} return o.join('')")
         try{ return data ? FN[tmpl](data) : FN[tmpl];}catch(e){ return e.toString(); }
     };
 })(window)
